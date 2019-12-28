6ec5e2f03f5b3de9182df383e340efc4a2795dcd
Bonnie Lai <bonnie_lai@acl.com>
2019-12-27 23:17:03 -0800

    Changing BoardExtractor from a class to a module such that it is easier to use any method by just passing a Trello instance

--- boardToCsv.js
+++ boardToCsv.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Trello = require('trello');
 const BoardExtractor = require('./src/boardExtractor');
 const ListToCsvWriter = require('./src/listToCsvWriter');
 
@@ -10,9 +11,9 @@ const token = process.env.TRELLO_TOKEN;
 const daOrgId = process.env.ORG_ID;
 
 async function writeBoardToCsv(boardName, filename) {
-  const boardExtractor = new BoardExtractor(key, token, daOrgId);
-  const boardId = await boardExtractor.getBoardIdByName(boardName);
-  const listsWithCards = await boardExtractor.getBoardListsWithCardsById(boardId);
+  const trello = new Trello(key, token);
+  const boardId = await BoardExtractor.getBoardIdByName(trello, daOrgId, boardName);
+  const listsWithCards = await BoardExtractor.getBoardListsWithCardsById(trello, boardId);
 
   const outputPath = path.resolve(process.cwd(), `${filename}.csv`);
   new ListToCsvWriter(listsWithCards).writeToCsv(outputPath);
--- src/boardExtractor.js
+++ src/boardExtractor.js
@@ -1,44 +1,62 @@
-const Trello = require('trello');
 const jp = require('jsonpath');
 
-class BoardExtractor {
-  constructor(key, token, orgId) {
-    this.trello = new Trello(key, token);
-    this.orgId = orgId;
-  }
+async function getBoardIdByName(trello, orgId, name) {
+  const boards = await trello.getOrgBoards(orgId);
+  const results = jp.query(boards, `$..[?(@.name=="${name}")]`);
 
-  async getBoardIdByName(name) {
-    const boards = await this.trello.getOrgBoards(this.orgId);
-    const results = jp.query(boards, `$..[?(@.name=="${name}")]`);
-
-    // TODO: should handle error with exception?
-    return results.length > 0 ? results[0].id : undefined;
-  }
-
-  async getBoardListsWithCardsById(id, opts = { withStickers: true, withComments: true }) {
-    const lists = await this.trello.getListsOnBoard(id);
-
-    const promises = lists.map(async list => {
-      return {
-        listName: list.name,
-        cards: await this.trello.getCardsOnList(list.id)
-      };
-    });
-    const listsWithCards = await Promise.all(promises);
-    return listsWithCards;
-  }
-
-  async getCardCommentsByCardId(id) {
-    const actions = await this.trello.makeRequest('get', `/1/cards/${id}/actions`, {
-      filter: 'commentCard',
-      format: 'list',
-      memberCreator: false,
-      member: false,
-      fields: 'data'
-    });
-
-    return actions.map(action => action.data.text);
-  }
+  // TODO: should handle error with exception?
+  return results.length > 0 ? results[0].id : undefined;
 }
 
-module.exports = BoardExtractor;
+async function getCardCommentsByCardId(trello, id) {
+  const actions = await trello.makeRequest('get', `/1/cards/${id}/actions`, {
+    filter: 'commentCard',
+    format: 'list',
+    memberCreator: false,
+    member: false,
+    fields: 'data'
+  });
+
+  return actions.map(action => action.data.text);
+}
+
+async function getCardsByListId(trello, list, isMinimal = true) {
+  const cards = await trello.getCardsOnList(list.id);
+  return {
+    listName: list.name,
+    cards: isMinimal
+      ? await Promise.all(
+          cards.map(async card => {
+            return {
+              name: card.name,
+              comments: await getCardCommentsByCardId(trello, card.id)
+            };
+          })
+        )
+      : cards
+  };
+}
+
+/**
+ * Fetch the cards of an entire board in relation to their respective lists given the board ID
+ *
+ * @param {*} id - Board ID
+ * @param {*} opts
+ *
+ * @returns {Array<Object>} - An array of lists with the following structure: { listName - {String}, cards - {Array<Object>} }
+ */
+async function getBoardListsWithCardsById(trello, id, isMinimal = true) {
+  const lists = await trello.getListsOnBoard(id);
+
+  const listsWithCards = await Promise.all(
+    lists.map(async list => getCardsByListId(trello, list, isMinimal))
+  );
+  return listsWithCards;
+}
+
+module.exports = {
+  getBoardIdByName,
+  getCardCommentsByCardId,
+  getCardsByListId,
+  getBoardListsWithCardsById
+};
